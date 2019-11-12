7ed824299bbf0300da9077d1e954c41feffbef53
Deniz Bektas <denisbektashtcone@gmail.com>
2019-11-12 15:30:47 +0300

    Добавьте файлы проекта.

---
+++ protect_inf_LR1/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace protect_inf_LR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonVizhener = new System.Windows.Forms.RadioButton();
+            this.radioButtonGamma = new System.Windows.Forms.RadioButton();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecipher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxKeyWord = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.groupBox1.Controls.Add(this.radioButtonVizhener);
+            this.groupBox1.Controls.Add(this.radioButtonGamma);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 1.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.ForeColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Location = new System.Drawing.Point(444, 73);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(13, 18);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод шифрования";
+            this.groupBox1.Visible = false;
+            // 
+            // radioButtonVizhener
+            // 
+            this.radioButtonVizhener.AutoSize = true;
+            this.radioButtonVizhener.Checked = true;
+            this.radioButtonVizhener.Location = new System.Drawing.Point(24, 54);
+            this.radioButtonVizhener.Name = "radioButtonVizhener";
+            this.radioButtonVizhener.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonVizhener.TabIndex = 1;
+            this.radioButtonVizhener.TabStop = true;
+            this.radioButtonVizhener.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.radioButtonVizhener.UseVisualStyleBackColor = false;
+            this.radioButtonVizhener.Visible = false;
+            // 
+            // radioButtonGamma
+            // 
+            this.radioButtonGamma.AutoSize = true;
+            this.radioButtonGamma.BackColor = System.Drawing.Color.Transparent;
+            this.radioButtonGamma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.radioButtonGamma.CheckAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.radioButtonGamma.Location = new System.Drawing.Point(24, 24);
+            this.radioButtonGamma.Name = "radioButtonGamma";
+            this.radioButtonGamma.Size = new System.Drawing.Size(14, 13);
+            this.radioButtonGamma.TabIndex = 0;
+            this.radioButtonGamma.UseVisualStyleBackColor = false;
+            this.radioButtonGamma.Visible = false;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonEncrypt.Location = new System.Drawing.Point(357, 11);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(112, 34);
+            this.buttonEncrypt.TabIndex = 1;
+            this.buttonEncrypt.Text = "Зашифровать";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecipher
+            // 
+            this.buttonDecipher.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDecipher.Location = new System.Drawing.Point(357, 108);
+            this.buttonDecipher.Name = "buttonDecipher";
+            this.buttonDecipher.Size = new System.Drawing.Size(112, 37);
+            this.buttonDecipher.TabIndex = 2;
+            this.buttonDecipher.Text = "Расшифровать";
+            this.buttonDecipher.UseVisualStyleBackColor = true;
+            this.buttonDecipher.Click += new System.EventHandler(this.buttonDecipher_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Garamond", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(27, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Введите ключ";
+            // 
+            // textBoxKeyWord
+            // 
+            this.textBoxKeyWord.Location = new System.Drawing.Point(30, 120);
+            this.textBoxKeyWord.Name = "textBoxKeyWord";
+            this.textBoxKeyWord.Size = new System.Drawing.Size(163, 20);
+            this.textBoxKeyWord.TabIndex = 4;
+            this.textBoxKeyWord.TextChanged += new System.EventHandler(this.textBoxKeyWord_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(31, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(162, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(30, 193);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 20);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Garamond", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Введите текст";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Garamond", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(27, 154);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 24);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Результат";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Garamond", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(357, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 47);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Скопировать и вставить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::protect_inf_LR1.Properties.Resources.dblogo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(503, 247);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxKeyWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDecipher);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Бекташ Д.Т. Гамирование по модулю N";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonVizhener;
+        private System.Windows.Forms.RadioButton radioButtonGamma;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecipher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxKeyWord;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ protect_inf_LR1/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+namespace protect_inf_LR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            N = 33;
+        }
+
+        char[] characters = new char[] { 'Я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                        'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 
+                                                        'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                        'Э', 'Ю', 'Я' };
+
+        private int N; //длина алфавита
+
+        //зашифровать
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if (radioButtonGamma.Checked)
+            {
+                string s;
+
+                StreamReader sr = new StreamReader("in.txt");
+                StreamWriter sw = new StreamWriter("out.txt");
+
+                while (!sr.EndOfStream)
+                {
+                    s = sr.ReadLine();
+                    sw.WriteLine(Encode(s, Generate_Pseudorandom_KeyWord(s.Length, 100)));
+                }
+
+                sr.Close();
+                sw.Close();
+            }
+
+            else
+            {
+                string lines = textBox1.Text;
+                string writePath = "in.txt";
+
+
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.UTF8))
+                {
+                    sw.WriteLine(lines);
+                }
+                if (textBoxKeyWord.Text.Length > 0)
+                {
+                    string s;
+
+                    StreamReader sr = new StreamReader("in.txt");
+                    StreamWriter sw = new StreamWriter("out.txt");
+
+                    while (!sr.EndOfStream)
+                    {
+                        s = sr.ReadLine();
+                        sw.WriteLine(Encode(s, textBoxKeyWord.Text));
+                    }
+
+                    sr.Close();
+                    sw.Close();
+                }
+                
+                else
+                    MessageBox.Show("Введите ключевое слово!");
+
+                
+                if(textBox1.Text!="")
+                textBox2.Text = File.ReadAllText("out.txt", Encoding.UTF8);
+                else
+                    MessageBox.Show("Введите  слово!");
+
+            }
+        }
+        
+    
+        //расшифровать
+        private void buttonDecipher_Click(object sender, EventArgs e)
+        {
+            if (radioButtonGamma.Checked)
+            {
+                string s;
+
+                StreamReader sr = new StreamReader("in.txt");
+                StreamWriter sw = new StreamWriter("out.txt");
+
+                while (!sr.EndOfStream)
+                {
+                    s = sr.ReadLine();
+                    sw.WriteLine(Decode(s, Generate_Pseudorandom_KeyWord(s.Length, 100)));
+                }
+
+                sr.Close();
+                sw.Close();
+            }
+            else
+            {
+                if (textBoxKeyWord.Text.Length > 0)
+                {
+                    string s;
+
+                    StreamReader sr = new StreamReader("in.txt");
+                    StreamWriter sw = new StreamWriter("out.txt");
+
+                    while (!sr.EndOfStream)
+                    {
+                        s = sr.ReadLine();
+                        sw.WriteLine(Decode(s, textBoxKeyWord.Text));
+                    }
+
+                    sr.Close();
+                    sw.Close();
+                }
+
+                else
+                    MessageBox.Show("Введите ключевое слово!");
+
+                
+                textBox2.Text = File.ReadAllText("out.txt", Encoding.UTF8);
+            }
+        }
+
+        //зашифровать
+        private string Encode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+
+            int keyword_index = 0;
+            
+                
+                foreach (char symbol in input)
+                {
+                try
+                {
+                    int c = (Array.IndexOf(characters, symbol) +
+                        Array.IndexOf(characters, keyword[keyword_index])) % N;
+                
+                    result += characters[c];
+                    keyword_index++;
+                }
+                catch (IndexOutOfRangeException ex)
+               { MessageBox.Show("Неверно ввел исходные данные", "Ошибка дурака"); }
+                
+
+                    if ((keyword_index ) == keyword.Length)
+                        keyword_index = 0;
+                }
+                
+            
+            return result;
+        }
+
+        //расшифровать
+        private string Decode(string input, string keyword)
+        {
+            input = input.ToUpper();
+            keyword = keyword.ToUpper();
+
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int p = (Array.IndexOf(characters, symbol) + N -
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[p];
+
+                keyword_index++;
+
+                if ((keyword_index ) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+        private string Generate_Pseudorandom_KeyWord(int lenght, int startSeed)
+        {
+            Random rand = new Random(startSeed);
+
+            string result = "";
+
+            for (int i = 0; i < lenght; i++)
+                result += characters[rand.Next(0, characters.Length)];
+
+            return result;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+           //     OpenFileDialog o = new OpenFileDialog();
+           //     o.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            //    if (o.ShowDialog() == DialogResult.OK)
+                {
+                    textBox2.Text = File.ReadAllText("out.txt", Encoding.UTF8);
+                }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = File.ReadAllText("out.txt", Encoding.UTF8);
+            string lines = textBox1.Text;
+            string writePath = "in.txt";
+
+
+            using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.UTF8))
+            {
+                sw.WriteLine(lines);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+            { 
+                string tempstr = "";
+                foreach (char sym in textBox1.Text)
+                {
+                    if (!TextBoxСheck(sym.ToString()))
+                        tempstr += sym.ToString();
+                }
+                textBox1.Text = tempstr;
+                textBox1.SelectionStart = textBox1.TextLength;
+            }
+
+             bool TextBoxСheck(string text)
+            {
+                return new Regex("[^А-Яа-я]").IsMatch(text);
+            }
+
+        }
+
+        private void textBoxKeyWord_TextChanged(object sender, EventArgs e)
+        {
+            {
+                string tempstr = "";
+                foreach (char sym in textBoxKeyWord.Text)
+                {
+                    if (!TextBoxСheck(sym.ToString()))
+                        tempstr += sym.ToString();
+                }
+                textBoxKeyWord.Text = tempstr;
+                textBoxKeyWord.SelectionStart = textBoxKeyWord.TextLength;
+            }
+
+            bool TextBoxСheck(string text)
+            {
+                return new Regex("[^А-Яа-я]").IsMatch(text);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
